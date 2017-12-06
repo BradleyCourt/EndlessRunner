89afa9c4bc3a900583b7695aefeae7b763031dbf
Bradley Court <bradleycourt@hotmail.com>
2017-12-06 15:50:22 +1030

    Added a new scene, added ball and tilting mechanics

---
+++ Assets/AccelerometerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerInput : MonoBehaviour {
+    Vector3 mousePos;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 translation = Vector3.zero;
+
+#if UNITY_ANDROID
+        translation = new Vector3(Input.acceleration.x, 0, -Input.acceleration.z);
+
+#else
+        if (transform.eulerAngles.x >-55 && transform.eulerAngles.x < 55) && (transform.eulerAngles.z >-55 && <55)
+        {
+            translation = mousePos - new Vector3(-Input.mousePosition.y, 0, Input.mousePosition.x); // Todo normalise to (-1,1) range
+            mousePos = new Vector3(-Input.mousePosition.y, 0, Input.mousePosition.x);
+        }
+        
+
+#endif
+        transform.Rotate(translation, Space.World);
+    }
+}
